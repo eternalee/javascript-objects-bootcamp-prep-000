0e0e1d717a73dd2b829cd250e3891213be99f394
Eterna <one.eternity@gmail.com>
2017-12-29 23:14:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artist, song) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
   return playlist
 }
