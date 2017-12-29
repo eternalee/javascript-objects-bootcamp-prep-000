0806bc88f0cea189df1231be8e98dbc17d9ed0cb
Eterna <one.eternity@gmail.com>
2017-12-29 23:13:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
 var playlist = { Tswizzle: "Blank Space", Pentatonix: 'Say Something'}
 
 function updatePlaylist(playlist, artist, song) {
-  [playlist] {artist: song}
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete
   return playlist
 }
